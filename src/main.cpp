dec02e681df73a14e95c4be8a05013265b682bd6
Laksh Setia <lakshsetia30@gmail.com>
2025-08-22 15:36:21 +0530

    added files

---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include "imgui.h"
+#include "imgui-SFML.h"
+
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode({640, 480}), "test");
+    bool imguiInit = ImGui::SFML::Init(window);
+
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    sf::Clock deltaClock;
+    while (window.isOpen()) {
+        while (std::optional<sf::Event> event = window.pollEvent()) {
+            ImGui::SFML::ProcessEvent(window, *event);
+            if (event->is<sf::Event::Closed>()) {
+                window.close();
+            }
+        }
+
+        ImGui::SFML::Update(window, deltaClock.restart());
+        ImGui::Begin("test");
+        ImGui::Button("Button");
+        ImGui::End();
+
+        window.clear();
+        window.draw(shape);
+        ImGui::SFML::Render(window);
+        window.display();
+    }
+
+    ImGui::SFML::Shutdown();
+}
